3c509b6ebd7e0c65a0da1872b02e8adf1fa2e371
Erica Little <eteney90@gmail.com>
2021-04-04 15:34:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,10 +31,10 @@ class Song
 
   def self.find_by_name(name)
     @@all.find { |song| song.name == name}
-  end 
+  end
 
-  def self.find_or_create_by_name(name) 
-    self.find_by_name(name) || self.create_by_name(name) 
-  end 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
 
 end
