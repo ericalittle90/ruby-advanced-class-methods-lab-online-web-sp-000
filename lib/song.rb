894dde9836d1d35979b7da7ba67f15258ea1304b
Erica Little <eteney90@gmail.com>
2021-04-04 15:56:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,11 +42,11 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    split_filename = filename.chomp(".mp3").split("-") 
-    song = Song.new 
-    song.name = split_filename[0] 
-    song.artist_name = split_filename[1] 
-    song 
+    split_filename = filename.chomp(".mp3").split("-")
+    song = Song.new
+    song.name = split_filename[0]
+    song.artist_name = split_filename[1]
+    song
   end
 
 end
